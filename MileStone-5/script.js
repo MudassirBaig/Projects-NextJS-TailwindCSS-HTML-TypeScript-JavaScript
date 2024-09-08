962c183bb4d4631ebda0f911c572975d93aba191
unknown <mb@vsolglobal.com>
2024-09-08 17:21:51 +0500

    updates added

--- MileStone-5/script.js
+++ MileStone-5/script.js
@@ -22,12 +22,14 @@ var _a;
         var rollNumer = RollElement.value;
         var qtr = QtrElement.value;
         var username = usernameElement.value;
-        var uniquepath = "resumes/".concat(username.replace(/\s+/g, '_'), "_cv.html");
+        //const uniquepath = `resumes/${username.replace(/\s+/g, '_')}_cv.pdf`
+        var formattedUsername = username.replace(/\s+/g, '_');
+        var uniquePath = "".concat(formattedUsername, "_cv.pdf");
         //create resume output
         var resumeOutput = "\n <h2>Resume</h2>\n <p><strong>Name:</strong> ".concat(name_1, "</p>\n <p><strong>Email:</strong> ").concat(email, " </p>\n <p><strong>Name:</strong> ").concat(phone, "</p>\n <p><strong>Rollnumber:</strong> ").concat(rollNumer, " </p>\n <p><strong>Quarter:</strong> ").concat(qtr, "</p>\n \n <h3>Education</h3>\n <p>").concat(education, "</p>\n \n <h3>Expereince</h3>\n <p>").concat(experience, "</p>\n \n <h3>Skills</h3>\n <p>").concat(skills, "</p>\n ");
         var downloadLinkk = document.createElement('a');
         downloadLinkk.href = 'data:text/html;charset=utf-8,' + encodeURIComponent(resumeOutput);
-        downloadLinkk.download = uniquepath;
+        downloadLinkk.download = uniquePath;
         downloadLinkk.textContent = 'Download your Resume';
         var resumeOutputElement = document.getElementById('resumeOutput');
         if (resumeOutputElement) {
