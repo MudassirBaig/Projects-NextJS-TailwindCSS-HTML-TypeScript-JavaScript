b8717487e5f50d52fefc81fa13ad3ae556ddd9e8
unknown <mb@vsolglobal.com>
2024-10-29 18:01:04 +0500

    Project added

--- PowerMonitor-website-custom-css/src/app/layout.tsx
+++ PowerMonitor-website-custom-css/src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import Contact from "@/app/contact";
+// import Contact from "@/app/contact";
 import Footer from "@/components/Footer";
 
 const geistSans = localFont({
